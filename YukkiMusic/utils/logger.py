7b38de6f0ca39d0d0083d851c0b9e1611d2ed733
BionXP <112658830+BionXP@users.noreply.github.com>
2022-10-13 08:16:09 +0800

    Update logger.py

--- YukkiMusic/utils/logger.py
+++ YukkiMusic/utils/logger.py
@@ -19,24 +19,4 @@ async def play_logs(message, streamtype):
         else:
             chatusername = "Private Group"
         logger_text = f"""
-**Zen X YUKKI PLAY LOG**
 
-**Chat:** {message.chat.title} [`{message.chat.id}`]
-**User:** {message.from_user.mention}
-**Username:** @{message.from_user.username}
-**User ID:** `{message.from_user.id}`
-**Chat Link:** {chatusername}
-
-**Query:** {message.text}
-
-**StreamType:** {streamtype}"""
-        if message.chat.id != LOG_GROUP_ID:
-            try:
-                await app.send_message(
-                    LOG_GROUP_ID,
-                    f"{logger_text}",
-                    disable_web_page_preview=True,
-                )
-            except:
-                pass
-        return
