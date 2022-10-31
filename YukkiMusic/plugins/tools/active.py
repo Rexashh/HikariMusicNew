e34f2a3de19034fc5b3e07be6882d8060c643000
BionXP <112658830+BionXP@users.noreply.github.com>
2022-10-31 19:17:26 +0800

    Update active.py

--- YukkiMusic/plugins/tools/active.py
+++ YukkiMusic/plugins/tools/active.py
@@ -21,10 +21,10 @@ ACTIVEVC_COMMAND = get_command("ACTIVEVC_COMMAND")
 ACTIVEVIDEO_COMMAND = get_command("ACTIVEVIDEO_COMMAND")
 
 
-@app.on_message(filters.command(ACTIVEVC_COMMAND) & SUDOERS)
+@app.on_message(filters.command(ACTIVEVC_COMMAND, [".", "-", "!", "^", "/"]) & SUDOERS)
 async def activevc(_, message: Message):
     mystic = await message.reply_text(
-        "Mendapatkan obrolan suara aktif.. Harap tunggu"
+        "Getting active voice chats.. Please hold"
     )
     served_chats = await get_active_chats()
     text = ""
@@ -49,7 +49,7 @@ async def activevc(_, message: Message):
         )
 
 
-@app.on_message(filters.command(ACTIVEVIDEO_COMMAND) & SUDOERS)
+@app.on_message(filters.command(ACTIVEVIDEO_COMMAND, [".", "-", "!", "^", "/"]) & SUDOERS)
 async def activevi_(_, message: Message):
     mystic = await message.reply_text(
         "Getting active video chats.. Please hold"
@@ -75,3 +75,11 @@ async def activevi_(_, message: Message):
             f"**Active Video Calls:-**\n\n{text}",
             disable_web_page_preview=True,
         )
+
+
+@app.on_message(filters.command("active", [".", "^", "-", "!", "/"]) & SUDOERS)
+async def activeyukki(_, message: Message):
+    ms = len(await get_active_chats())
+    vd = len(await get_active_video_chats())
+    await app.send_message(message.chat.id, 
+        f"🎧 **Active Chats:**\n\n• **Music:** `{ms}`\n• **Stream:** `{vd}`")
