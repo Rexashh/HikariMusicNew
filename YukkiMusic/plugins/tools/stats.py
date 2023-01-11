1247fd0690c3fcacb62060761f1170a39e6bc6f4
BionXP <112658830+BionXP@users.noreply.github.com>
2023-01-11 09:29:42 +0800

    Update stats.py

--- YukkiMusic/plugins/tools/stats.py
+++ YukkiMusic/plugins/tools/stats.py
@@ -23,7 +23,7 @@ from config import BANNED_USERS, MUSIC_BOT_NAME
 from strings import get_command
 from YukkiMusic import YouTube, app
 from YukkiMusic.core.userbot import assistants
-from YukkikMusic.misc import SUDOERS, pymongodb
+from YukkiMusic.misc import SUDOERS, pymongodb
 from YukkiMusic.plugins import ALL_MODULES
 from YukkiMusic.utils.database import (get_global_tops,
                                        get_particulars, get_queries,
