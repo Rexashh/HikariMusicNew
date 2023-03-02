81ed100e1fb37cc8562620fe19a36e222130f4e6
Hikari Kawai <121547505+hikarikawai@users.noreply.github.com>
2023-03-02 21:49:51 +0700

    Update logger.py

--- YukkiMusic/utils/logger.py
+++ YukkiMusic/utils/logger.py
@@ -19,7 +19,7 @@ async def play_logs(message, streamtype):
         else:
             chatusername = "Private Group"
         logger_text = f"""
-📮 **ZEN PLAY LOG**
+🎸 **HIKARI PLAY LOG**
 
 💬 **Chat:** {message.chat.title} [`{message.chat.id}`]
 👨🏻‍🚀 **User:** {message.from_user.mention}
